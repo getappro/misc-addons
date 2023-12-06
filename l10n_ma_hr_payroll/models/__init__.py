f33bea451aaae55fc8e89dab0418afc17c4feae8
GetApPRO <81932927+getappro@users.noreply.github.com>
2023-12-06 01:03:04 +0100

    Add files via upload

---
+++ l10n_ma_hr_payroll/models/__init__.py
@@ -0,0 +1,14 @@
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from . import res_company
+from . import hr_employee
+from . import hr_contract
+from . import l10n_ma_hr_payroll
